42b47f09130a03ead2bc30f58f894a2d50b58723
laxmishinde29 <laxmishinde2975@gmail.com>
2025-07-15 11:33:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (81.90%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,32 +1,40 @@
-class Solution {
-    public :
-    int myAtoi(string s) {
-        if(s.length()==0) return 0;
 
-        int i=0;
-         while(i<s.size() && s[i]== ' ') {
-             i++;
-         }     
-        s = s.substr(i);
+class Solution {
+public:
+    int myAtoi(string str) {
+        if( str.empty())
+            return 0;
+        int len = str.length(), i=0, sign = 1;
         
-        int sign = +1;
-        long ans = 0;
+        while( i<len && str[i] == ' ')
+            i++;
         
-        if(s[0] == '-') sign = -1;
+        if(i==len)
+            return 0;
         
-        int MAX = INT_MAX, MIN = INT_MIN;
-        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
-		
-        while(i < s.length()) {
-            if(s[0] == ' ' || !isdigit(s[i])) break;
-            
-            ans = ans * 10 + s[i]-'0';
-            if(sign == -1 && -1*ans < MIN) return MIN;
-            if(sign == 1 && ans > MAX) return MAX;
-            
+        if(str[i] == '-'){
+            sign = 0;
+            i++;
+        }
+        else if(str[i] == '+')
+            i++;
+        
+        long int out = 0;
+        
+        while(str[i] >= '0' && str[i] <= '9'){
+            out = out * 10;
+            if(out <= INT_MIN || out >= INT_MAX)
+                break;
+            out = out + (str[i] - '0');
             i++;
         }
         
-        return (int)(sign*ans);
+        if(sign == 0)
+            out = -1 * out;
+        if(out <= INT_MIN)
+            return INT_MIN;
+        if(out >= INT_MAX)
+            return INT_MAX;
+        return (int)out;
     }
 };
